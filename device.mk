cf9c27618441962b436ff1dc7855945851e479fd
johnmart19 <johnivan19999@gmail.com>
2022-11-02 02:08:10 +0200

    Alioth: Nuke OTA Preopt Script and Flags
    - OTA Dexopt optimizations taking a lot of time, let's skip it on Installation moment
    
    Change-Id: I0d9fa878de6e538212c7bcc8da4aa5611af3db05

--- device.mk
+++ device.mk
@@ -16,20 +16,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression_with_xor.mk)
 
 AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
     FILESYSTEM_TYPE_vendor=ext4 \
     POSTINSTALL_OPTIONAL_vendor=true
-
-# OTA Preopt Script
-PRODUCT_PACKAGES += \
-    otapreopt_script
 
 # Userdata Checkpointing OTA GC
 PRODUCT_PACKAGES += \
